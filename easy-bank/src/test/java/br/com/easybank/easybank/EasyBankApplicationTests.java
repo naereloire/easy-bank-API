fa7310613409caba3c4d3b090a8b890750bd7837
Naere Loire Guimarães dos Santos <CIANDT\naere@lnb021851spo.ciandt.global>
2021-01-10 14:49:58 -0300

    spring init

---
+++ easy-bank/src/test/java/br/com/easybank/easybank/EasyBankApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.easybank.easybank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
